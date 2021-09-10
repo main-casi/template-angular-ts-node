9b4a89cf88325d86c4039d33ef806a39c75c6e86
Aaron Carneiro <euaaron@outlook.com>
2021-09-10 12:48:34 -0300

    test: broke the code to test husky

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Router = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
